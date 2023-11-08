373aaa51d94dd7e7a15ca9a0c8cb04538b82f64f
Jessiel Chasiguano <chasiguano.jessiel@gmail.com>
2023-11-08 00:18:15 -0500

    Implementation of the logical NOT gate in Python.

---
+++ not.py
@@ -0,0 +1,6 @@
+print("para saber si pasa el semestre ingrese la nota")
+numero=int(input("introduce un valor = 7: "))
+if not numero <=7:
+      print("\n el valor es mayor de 7, si pasa.\n")
+else:
+      print("\n el valor  es menor de 7, NO pasa.\n")
